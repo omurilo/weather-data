b028ff8fbd6bc051b0b40cf57320e0420b9e06b3
Murilo Henrique <hi@omurilo.dev>
2022-02-04 09:29:01 -0300

    feat: create header alert component

---
+++ src/components/HeaderAlert/index.tsx
@@ -0,0 +1,9 @@
+import * as Styled from "./styles";
+
+export default function HeaderAlert() {
+  return (
+    <Styled.Alert>
+      <strong>Autorize a utilização da sua localização</strong>
+    </Styled.Alert>
+  );
+}
---
+++ src/components/HeaderAlert/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Alert = styled.div`
+  position: fixed;
+  background: #f00;
+  width: 100%;
+  height: 50px;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1rem;
+  color: #FFF;
+`;
